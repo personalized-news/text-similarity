ca940f98d5bef3b778640b25659be30621c741a5
ZYSzys <17367077526@163.com>
2019-02-11 17:27:44 +0800

    python env

--- Construct word vector/Participle.py
+++ Construct word vector/Participle.py
@@ -6,7 +6,7 @@ output = open('words.txt', 'w', encoding="utf-8")
 with open('context.txt', encoding='utf-8',  mode = 'r') as f:
     for line in f:
         seg_list = jieba.cut(line)
-        output.write(" ".join(seg_list) + '\n')
+        output.write(" ".join(seg_list))
 
 f.close()
 output.close()
